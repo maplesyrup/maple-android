c787756ca0e079a6ddf4ebdb9b93e1d0b4d2a3f5
Eli Hart <elihart@stanford.edu>
2013-03-16 20:37:54 -0700

    Added Android Asynchronous Http Client library and created MapleHttpClient class to provide easy get and post requests

---
+++ src/com/example/maple_android/MapleHttpClient.java
@@ -0,0 +1,20 @@
+package com.example.maple_android;
+import com.loopj.android.http.*;
+
+public class MapleHttpClient {
+	 private static final String BASE_URL = "http://maplesyrup.herokuapp.com";
+ 
+	  private static AsyncHttpClient client = new AsyncHttpClient();
+
+	  public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+	      client.get(getAbsoluteUrl(url), params, responseHandler);
+	  }
+
+	  public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+	      client.post(getAbsoluteUrl(url), params, responseHandler);
+	  }
+
+	  private static String getAbsoluteUrl(String relativeUrl) {
+	      return BASE_URL + relativeUrl;
+	  }
+}
