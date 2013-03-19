338d62a9730a45f30ea32b3f9cdf0372f115533a
Eli Hart <elihart@stanford.edu>
2013-03-19 14:10:32 -0700

    FontPickerDialog created
    Dialog shows all available font options. Selected font isn't yet saved
    though.

--- src/com/example/maple_android/FontPickerDialog.java
+++ src/com/example/maple_android/FontPickerDialog.java
@@ -1,146 +1,174 @@
-/*
- * Copyright (C) 2011-2012 George Yunaev
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- */
-
 package com.example.maple_android;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import android.app.AlertDialog.Builder;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences.Editor;
+import android.content.DialogInterface.OnClickListener;
 import android.graphics.Typeface;
-import android.preference.DialogPreference;
-import android.util.AttributeSet;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckedTextView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class FontPickerDialog extends DialogPreference implements DialogInterface.OnClickListener
-{
+public class FontPickerDialog extends DialogFragment {
 	// Keeps the font file paths and names in separate arrays
-	private List< String >	m_fontPaths;
-	private List< String >	m_fontNames;
+	private List<String> m_fontPaths;
+	private List<String> m_fontNames;
+	private Context mContext;
 
-	// Font adaptor responsible for redrawing the item TextView with the appropriate font.
-	// We use BaseAdapter since we need both arrays, and the effort is quite small.
-	public class FontAdapter extends BaseAdapter
-	{
+	// create callback method on font selected
+	public interface FontPickerDialogListener {
+		public void onFontSelected(DialogFragment dialog);
+	}
+
+	// Use this instance of the interface to deliver action events
+	FontPickerDialogListener mListener;
+
+	// Override the Fragment.onAttach() method to instantiate the
+	// Listener
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		// Verify that the host activity implements the callback interface
+		try {
+			// Instantiate the NoticeDialogListener so we can send events to the
+			// host
+			mListener = (FontPickerDialogListener) activity;
+		} catch (ClassCastException e) {
+			// The activity doesn't implement the interface, throw exception
+			throw new ClassCastException(activity.toString()
+					+ " must implement FontPickerDialogListener");
+		}
+	}
+
+	public static FontPickerDialog newInstance() {
+		FontPickerDialog frag = new FontPickerDialog();
+
+		return frag;
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// get context
+		mContext = getActivity();
+
+		// Let FontManager find available fonts
+		HashMap<String, String> fonts = FontManager.enumerateFonts();
+		m_fontPaths = new ArrayList<String>();
+		m_fontNames = new ArrayList<String>();
+
+		// add fonts to List
+		for (String path : fonts.keySet()) {
+			m_fontPaths.add(path);
+			m_fontNames.add(fonts.get(path));
+		}
+
+		// Instantiate an AlertDialog.Builder with its constructor
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+		// Get the layout inflater
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+
+		// Inflate and set the layout for the dialog
+		// Pass null as the parent view because its going in the dialog layout
+		builder.setView(inflater.inflate(R.layout.font_picker_dialog, null));
+
+		// set adapter
+		FontAdapter adapter = new FontAdapter(mContext);
+
+		builder.setAdapter(adapter, new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
+
+		builder.setTitle("Font");
+
+		// Add the buttons
+		builder.setPositiveButton(R.string.ok,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// Send the positive button event back to the host activity
+	                       mListener.onFontSelected(FontPickerDialog.this);
+					}
+				});
+
+		// Get the AlertDialog from create()
+		AlertDialog dialog = builder.create();
+
+		return dialog;
+	}
+
+	public class FontAdapter extends BaseAdapter {
+		private Context mContext;
+
+		public FontAdapter(Context c) {
+			mContext = c;
+		}
+
 		@Override
-		public int getCount()
-		{
+		public int getCount() {
 			return m_fontNames.size();
 		}
 
 		@Override
-		public Object getItem(int position)
-		{
-			return m_fontNames.get( position );
+		public Object getItem(int position) {
+			return m_fontNames.get(position);
 		}
 
 		@Override
-		public long getItemId(int position)
-		{
+		public long getItemId(int position) {
 			// We use the position as ID
 			return position;
 		}
 
 		@Override
-		public View getView( int position, View convertView, ViewGroup parent )
-		{
-            View view = convertView;
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView view = (TextView) convertView;
 
-            // This function may be called in two cases: a new view needs to be created,
-            // or an existing view needs to be reused
-            if ( view == null )
-            {
-            	// Since we're using the system list for the layout, use the system inflater
-            	final LayoutInflater inflater = (LayoutInflater)
-                            getContext().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+			// This function may be called in two cases: a new view needs to be
+			// created,
+			// or an existing view needs to be reused
+			if (view == null) {
+				view = new TextView(mContext);
 
-            	// And inflate the view android.R.layout.select_dialog_singlechoice
-            	// Why? See com.android.internal.app.AlertController method createListView()
-            	view = inflater.inflate( android.R.layout.select_dialog_singlechoice, parent, false);
-            }
+			} else {
+				view = (TextView) convertView;
+			}
 
-            if ( view != null )
-            {
-            	// Find the text view from our interface
-            	CheckedTextView tv = (CheckedTextView) view.findViewById( android.R.id.text1 );
+			// Replace the string with the current font name using our
+			// typeface
+			Typeface tface = Typeface.createFromFile(m_fontPaths.get(position));
+			view.setTypeface(tface);
 
-            	// Replace the string with the current font name using our typeface
-            	Typeface tface = Typeface.createFromFile( m_fontPaths.get( position ) );
-            	tv.setTypeface( tface );
+			// If you want to make the selected item having different
+			// foreground or background color,
+			// be aware of themes. In some of them your foreground color
+			// may
+			// be the background color.
+			// So we don't mess with anything here and just add the
+			// extra
+			// stars to have the selected
+			// font to stand out.
+			view.setText(m_fontNames.get(position));
 
-            	// If you want to make the selected item having different foreground or background color,
-            	// be aware of themes. In some of them your foreground color may be the background color.
-            	// So we don't mess with anything here and just add the extra stars to have the selected
-            	// font to stand out.
-            	tv.setText( m_fontNames.get( position ) );
-            }
+			return view;
 
-            return view;
 		}
 	}
-
-	public FontPickerDialog( Context context, AttributeSet attrs )
-	{
-		super(context, attrs);
-	}
-
-	@Override
-	protected void onPrepareDialogBuilder( Builder builder )
-	{
-        super.onPrepareDialogBuilder(builder);
-
-		// Get the fonts on the device
-		HashMap< String, String > fonts = FontManager.enumerateFonts();
-		m_fontPaths = new ArrayList< String >();
-		m_fontNames = new ArrayList< String >();
-
-		// Get the current value to find the checked item
-		String selectedFontPath = getSharedPreferences().getString( getKey(), "");
-		int idx = 0, checked_item = 0;
-
-		for ( String path : fonts.keySet() )
-		{
-			if ( path.equals( selectedFontPath ) )
-				checked_item = idx;
-
-			m_fontPaths.add( path );
-			m_fontNames.add( fonts.get(path) );
-			idx++;
-		}
-
-		// Create our adapter
-		// If you're building for API 11 and up, you can pass builder.getContext
-		// instead of current context
-		FontAdapter adapter = new FontAdapter();
-
-        builder.setSingleChoiceItems( adapter, checked_item, this );
-
-        // The typical interaction for list-based dialogs is to have click-on-an-item dismiss the dialog
-        builder.setPositiveButton(null, null);
-	}
-
-    public void onClick(DialogInterface dialog, int which)
-    {
-    	if ( which >=0 && which < m_fontPaths.size() )
-    	{
-    		String selectedFontPath = m_fontPaths.get( which );
-    		Editor editor = getSharedPreferences().edit();
-    		editor.putString( getKey(), selectedFontPath );
-    		editor.commit();
-
-    		dialog.dismiss();
-    	}
-    }
 }
--- src/com/example/maple_android/TextActivity.java
+++ src/com/example/maple_android/TextActivity.java
@@ -1,8 +1,14 @@
 package com.example.maple_android;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,26 +18,31 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TextView.BufferType;
 
 import com.example.maple_android.ColorPickerDialog.OnColorChangedListener;
 
-public class TextActivity extends Activity {
+public class TextActivity extends Activity implements FontPickerDialog.FontPickerDialogListener{
 	/* Global app */
 	MapleApplication app;
-	
+
 	private byte[] byteArray;
 	private ImageView photo;
 	private String companyTag;
 	private Bitmap srcBitmap;
-	
+
 	// text option
 	private boolean showOptions;
 	private float text_x;
@@ -47,60 +58,65 @@ public class TextActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_text);
-		
-		//Init app
-		app = (MapleApplication)this.getApplication();
-		
+
+		// Init app
+		app = (MapleApplication) this.getApplication();
+
 		// get company name
 		companyTag = app.getCurrentCompany();
-		
-		// get picture 
+
+		// get picture
 		Bundle extras = getIntent().getExtras();
 		byteArray = extras.getByteArray("photoByteArray");
 		filePath = extras.getString("filePath");
-		srcBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+		srcBitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+				byteArray.length);
 
 		// set photo
-		photo = (ImageView)this.findViewById(R.id.photo);
-        photo.setImageBitmap(srcBitmap);
-        
-        
-        // initialize photo for clicking
-        photo.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return placeText(v, event); 	            	
-            }			
-        });
-        
-        // start off not showing edit options
-        showOptions = false;
-        
-        // get TextView to overlap on photo
-        photoText = (TextView) findViewById(R.id.photoText);
-     	updateTextSize(); // initialize size to default
-        
-     	// set up text color
-     	textColor = photoText.getTextColors().getDefaultColor();
-     	((TextView) findViewById(R.id.changeColor)).setTextColor(textColor);
-     	
-     	// set up font size listener
-     	((EditText)findViewById(R.id.fontSize)).addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            	updateTextSize();
-            }
+		photo = (ImageView) this.findViewById(R.id.photo);
+		photo.setImageBitmap(srcBitmap);
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+		// initialize photo for clicking
+		photo.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return placeText(v, event);
+			}
+		});
 
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-        
-     	// set up text listener
-        textEntry = (EditText)this.findViewById(R.id.textEntry);
-        textEntry.addTextChangedListener(new TextWatcher() {
+		// start off not showing edit options
+		showOptions = false;
+
+		// get TextView to overlap on photo
+		photoText = (TextView) findViewById(R.id.photoText);
+		updateTextSize(); // initialize size to default
+
+		// set up text color
+		textColor = photoText.getTextColors().getDefaultColor();
+		((TextView) findViewById(R.id.changeColor)).setTextColor(textColor);
+
+		// set up font size listener
+		((EditText) findViewById(R.id.fontSize))
+				.addTextChangedListener(new TextWatcher() {
+					@Override
+					public void onTextChanged(CharSequence s, int start,
+							int before, int count) {
+						updateTextSize();
+					}
+
+					@Override
+					public void beforeTextChanged(CharSequence s, int start,
+							int count, int after) {
+					}
+
+					@Override
+					public void afterTextChanged(Editable s) {
+					}
+				});
+
+		// set up text listener
+		textEntry = (EditText) this.findViewById(R.id.textEntry);
+		textEntry.addTextChangedListener(new TextWatcher() {
 
 			@Override
 			public void afterTextChanged(Editable e) {
@@ -109,82 +125,92 @@ public class TextActivity extends Activity {
 
 			@Override
 			public void beforeTextChanged(CharSequence arg0, int arg1,
-					int arg2, int arg3) {}
+					int arg2, int arg3) {
+			}
 
 			@Override
 			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
-					int arg3) {	}       	
-        	
-        });
-        
-	}
-	
-	public void changeFontStyle(View view){
-		/* Android uses a really dumb sysem where you can't individually specify bold and italic,
-		 * if you want both there is a different setting for it. Makes this a little messier.
-		 */
-		
-		CheckBox bold = (CheckBox)findViewById(R.id.bold);
-		CheckBox italic = (CheckBox)findViewById(R.id.italic);			
-		
-		if(bold.isChecked() && italic.isChecked()) photoText.setTypeface(null, Typeface.BOLD_ITALIC);
-		else if(bold.isChecked()) photoText.setTypeface(null, Typeface.BOLD);
-		else if(italic.isChecked()) photoText.setTypeface(null, Typeface.ITALIC);
-		else photoText.setTypeface(Typeface.DEFAULT);
+					int arg3) {
+			}
+
+		});
 
 	}
-	
+
+	public void changeFontStyle(View view) {
+		/*
+		 * Android uses a really dumb sysem where you can't individually specify
+		 * bold and italic, if you want both there is a different setting for
+		 * it. Makes this a little messier.
+		 */
+
+		CheckBox bold = (CheckBox) findViewById(R.id.bold);
+		CheckBox italic = (CheckBox) findViewById(R.id.italic);
+
+		if (bold.isChecked() && italic.isChecked())
+			photoText.setTypeface(null, Typeface.BOLD_ITALIC);
+		else if (bold.isChecked())
+			photoText.setTypeface(null, Typeface.BOLD);
+		else if (italic.isChecked())
+			photoText.setTypeface(null, Typeface.ITALIC);
+		else
+			photoText.setTypeface(Typeface.DEFAULT);
+
+	}
+
 	// grabs the user entered value from the font size entry
 	// text box and updates the text size with it
 	private void updateTextSize() {
 		// grab entry as string
-		String textSize = ((EditText)findViewById(R.id.fontSize)).getText().toString();
-		
+		String textSize = ((EditText) findViewById(R.id.fontSize)).getText()
+				.toString();
+
 		// try to convert to int
 		Integer size = null;
 		try {
-		    size = Integer.valueOf(textSize);
-		  } catch (NumberFormatException e) {}
-		
+			size = Integer.valueOf(textSize);
+		} catch (NumberFormatException e) {
+		}
+
 		// update size if possible
-		if(size != null) photoText.setTextSize((float)size);
+		if (size != null)
+			photoText.setTextSize((float) size);
 	}
 
 	private boolean placeText(View v, MotionEvent event) {
 		// update options after click
-		if(!showOptions) toggleOptions();
-		
+		if (!showOptions)
+			toggleOptions();
+
 		// save click location
 		text_x = event.getX();
 		text_y = event.getY();
-		
+
 		// place textView
 		photoText.setX(text_x + v.getX());
 		photoText.setY(text_y + v.getY() - photoText.getBaseline());
-		
+
 		// set focus to text edit
 		textEntry.setFocusable(true);
 		textEntry.requestFocus();
-		
-		
+
 		return true;
 	}
-	
-	private void toggleOptions(){
+
+	private void toggleOptions() {
 		// switch setting
 		showOptions = !showOptions;
-		
+
 		// get int value for visibility setting
 		int visibility;
-		if(showOptions){
+		if (showOptions) {
 			visibility = View.VISIBLE;
 			findViewById(R.id.textInstructions).setVisibility(View.GONE);
-		}
-		else {
+		} else {
 			visibility = View.INVISIBLE;
 			findViewById(R.id.textInstructions).setVisibility(View.VISIBLE);
 		}
-		
+
 		// update View visibilities
 		findViewById(R.id.changeColor).setVisibility(visibility);
 		findViewById(R.id.save).setVisibility(visibility);
@@ -194,27 +220,39 @@ public class TextActivity extends Activity {
 		findViewById(R.id.bold).setVisibility(visibility);
 		findViewById(R.id.italic).setVisibility(visibility);
 		textEntry.setVisibility(visibility);
-		photoText.setVisibility(visibility);		
+		photoText.setVisibility(visibility);
+	}
+
+	// start a dialog that shows all availabe fonts and
+	// allows the user to pick which one they want to use
+	public void changeFont(View view) {
+		FontPickerDialog dlg = new FontPickerDialog();
+		dlg.show(getFragmentManager(), "font_picker");
 	}
 	
-	public void changeFont(View view){
-		FontPickerDialog fontPicker = new FontPickerDialog(this, null);
-		fontPicker.getDialog().show();
+	// call back method when a font has been selected
+	@Override
+	public void onFontSelected(DialogFragment dialog) {
+		
 	}
+
 	
-	public void changeColor(View view){
-		 OnColorChangedListener l = new OnColorChangedListener(){
+
+	public void changeColor(View view) {
+		OnColorChangedListener l = new OnColorChangedListener() {
 
 			@Override
 			public void colorChanged(int color) {
 				textColor = color;
 				photoText.setTextColor(color);
-				((TextView) findViewById(R.id.photoText)).setTextColor(textColor);
-			}			 
-		 };
-		
-		ColorPickerDialog colorPicker = new ColorPickerDialog(view.getContext(), l, textColor);
-		colorPicker.show();		
+				((TextView) findViewById(R.id.photoText))
+						.setTextColor(textColor);
+			}
+		};
+
+		ColorPickerDialog colorPicker = new ColorPickerDialog(
+				view.getContext(), l, textColor);
+		colorPicker.show();
 	}
 
 	@Override
@@ -223,41 +261,44 @@ public class TextActivity extends Activity {
 		getMenuInflater().inflate(R.menu.text, menu);
 		return true;
 	}
-	
-	
-	public void save(View view){		
+
+	public void save(View view) {
 		// get text bitmap
 		Bitmap textBitmap = loadBitmapFromView(photoText);
-		
+
 		// combine two bitmaps
-		Bitmap bmOverlay = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), srcBitmap.getConfig());
-        Canvas canvas = new Canvas(bmOverlay);
-        canvas.drawBitmap(srcBitmap, new Matrix(), null);
-        canvas.drawBitmap(textBitmap, text_x, text_y - photoText.getHeight(), null);
-        
+		Bitmap bmOverlay = Bitmap.createBitmap(srcBitmap.getWidth(),
+				srcBitmap.getHeight(), srcBitmap.getConfig());
+		Canvas canvas = new Canvas(bmOverlay);
+		canvas.drawBitmap(srcBitmap, new Matrix(), null);
+		canvas.drawBitmap(textBitmap, text_x, text_y - photoText.getHeight(),
+				null);
+
 		// save picture to byte array and return
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();        
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		bmOverlay.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byteArray = stream.toByteArray();
-        
-        returnToEditor(view);        
+		byteArray = stream.toByteArray();
+
+		returnToEditor(view);
 	}
-	
-	private Bitmap loadBitmapFromView(View v) {		
-		Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);                
-	    Canvas c = new Canvas(b);
-	    v.layout(0, 0, v.getWidth(), v.getHeight());
-	    v.draw(c);
-	    return b;
+
+	private Bitmap loadBitmapFromView(View v) {
+		Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
+				Bitmap.Config.ARGB_8888);
+		Canvas c = new Canvas(b);
+		v.layout(0, 0, v.getWidth(), v.getHeight());
+		v.draw(c);
+		return b;
 	}
-	
-	
-	public void returnToEditor(View view){
+
+	public void returnToEditor(View view) {
 		Intent i = new Intent(this, EditorActivity.class);
 		i.putExtra("photoByteArray", byteArray);
 		i.putExtra("filePath", filePath);
 
 		startActivity(i);
 	}
+
+
 
 }
