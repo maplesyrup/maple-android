3f17b71b3c169757e543c31d3e18e2535bde2b77
Rahul Pandey <rpandey1234@gmail.com>
2013-04-14 04:47:33 -0700

    combine common methods of personal and popular ads into new class. Improve UI

--- src/com/example/browsing/ImageAdapter.java
+++ src/com/example/browsing/ImageAdapter.java
@@ -6,13 +6,14 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.maple_android.R;
@@ -47,86 +48,40 @@ public class ImageAdapter extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-    	LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View adView = inflater.inflate(R.layout.ad_view, null);
-        final ImageView imageView = new ImageView(mContext);
-//    	final ImageView imageView = (ImageView) adView.findViewById(R.id.ad);
-    	
-        imageView.setLayoutParams(new GridView.LayoutParams(185, 185));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setPadding(8, 8, 8, 8);
-    	
-    	// Example image:
-        //http://s3.amazonaws.com/maplesyrup-assets/posts/images/000/000/006/medium/IMG_20130311_233546.jpg?1363070132
-        String url = "drawable://" + R.drawable.maple;
-        String title = "";
-        try {
-        	JSONObject jObject = ads.getJSONObject(position); 
-        	url = jObject.getString("image_url");
-	        title = jObject.getString("title");
-        } catch (JSONException e){
-        	Log.d(TAG, "unable to parse JSON");
-        }
-        ImageLoader imageLoader = ImageLoader.getInstance();
-        imageLoader.loadImage(url, new SimpleImageLoadingListener() {
-        	@Override
-        	public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-        		imageView.setImageBitmap(loadedImage);
-        	}
-        });
+    	View adView = convertView;
+        
+        if (convertView == null) {
+        	LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        	adView = inflater.inflate(R.layout.ad_view, null);
 
-    	TextView textView = (TextView) adView.findViewById(R.id.adInfo);
-    	textView.setText(title);
-        return imageView;
+        	final ImageView imageView = (ImageView) adView.findViewById(R.id.ad);
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(294, 294));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+
+        	
+        	// Example image:
+            //http://s3.amazonaws.com/maplesyrup-assets/posts/images/000/000/006/medium/IMG_20130311_233546.jpg?1363070132
+            String url = "drawable://" + R.drawable.maple;
+            String title = "";
+            try {
+            	JSONObject jObject = ads.getJSONObject(position); 
+            	url = jObject.getString("image_url");
+    	        title = jObject.getString("title");
+            } catch (JSONException e){
+            	Log.d(TAG, "unable to parse JSON");
+            }
+            ImageLoader imageLoader = ImageLoader.getInstance();
+            imageLoader.loadImage(url, new SimpleImageLoadingListener() {
+            	@Override
+            	public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            		imageView.setImageBitmap(loadedImage);
+            	}
+            });
+        	TextView textView = (TextView) adView.findViewById(R.id.adInfo);
+        	textView.setText(title);
+            textView.setBackgroundColor(Color.RED);
+        }
+        return adView;
     }
 }
-
-//public class ImageAdapter extends BaseAdapter {
-//    private Context mContext;
-//
-//    public ImageAdapter(Context c) {
-//        mContext = c;
-//    }
-//
-//    public int getCount() {
-//        return mThumbIds.length;
-//    }
-//
-//    public Object getItem(int position) {
-//        return null;
-//    }
-//
-//    public long getItemId(int position) {
-//        return 0;
-//    }
-//
-//    // create a new ImageView for each item referenced by the Adapter
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        ImageView imageView;
-//        if (convertView == null) {  // if it's not recycled, initialize some attributes
-//            imageView = new ImageView(mContext);
-//            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//            imageView.setPadding(8, 8, 8, 8);
-//        } else {
-//            imageView = (ImageView) convertView;
-//        }
-//
-//        imageView.setImageResource(mThumbIds[position]);
-//        return imageView;
-//    }
-//
-//    // references to our images
-//    private Integer[] mThumbIds = {
-//            R.drawable.cs210, R.drawable.maple,
-//            R.drawable.com_facebook_loginbutton_blue_normal, R.drawable.cs210,
-//            R.drawable.cs210, R.drawable.maple,
-//            R.drawable.com_facebook_loginbutton_blue_normal, R.drawable.cs210,
-//            R.drawable.cs210, R.drawable.maple,
-//            R.drawable.com_facebook_loginbutton_blue_normal, R.drawable.cs210,
-//            R.drawable.cs210, R.drawable.maple,
-//            R.drawable.com_facebook_loginbutton_blue_normal, R.drawable.cs210,
-//            R.drawable.cs210, R.drawable.maple
-//    };
-//}
-
--- src/com/example/browsing/PopularAdsActivity.java
+++ src/com/example/browsing/PopularAdsActivity.java
@@ -1,50 +1,12 @@
 package com.example.browsing;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
-import android.widget.Toast;
 
-import com.example.maple_android.R;
-import com.example.maple_android.Utility;
-
-public class PopularAdsActivity extends Activity {
+public class PopularAdsActivity extends BrowseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_popular_ads);
-
-	    GridView gridview = (GridView) findViewById(R.id.gridview);
-	    JSONArray jObjectAds = new JSONArray();
-	    gridview.setAdapter(new ImageAdapter(this, jObjectAds));
-
-	    gridview.setOnItemClickListener(new OnItemClickListener() {
-	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	            Toast.makeText(PopularAdsActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-	        }
-	    });
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-	
-	/**
-	 * Respond to each tab button
-	 */
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return Utility.myOnOptionsItemSelected(this, item);
+		super.requestUserAds(null);
 	}
 }
