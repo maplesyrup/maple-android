6adf78a64e03db0046177007c66d6c1dda10161b
Ben Rudolph <rudolphben@gmail.com>
2013-05-11 15:24:03 -0700

    Fix for my phone!

--- src/com/additt/maple_android/LoginActivity.java
+++ src/com/additt/maple_android/LoginActivity.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,7 +28,11 @@ public class LoginActivity extends Activity {
 		setContentView(R.layout.activity_login);
 		
 		// hide action bar for login page
-		getActionBar().hide();
+		try {
+			getActionBar().hide();
+		} catch (NoSuchMethodError e) {
+			Log.d(TAG, "Well, not high enough SDK for action bar so it won't be shown anyways");
+		}
 
 		Button buttonLoginLogout = (Button) findViewById(R.id.loginB);
 		buttonLoginLogout.setOnClickListener(new OnClickListener() {
