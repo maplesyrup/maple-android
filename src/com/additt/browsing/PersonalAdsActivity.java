33fe09f81516d5bb8e665e5313fd2cec094e6fca
Rahul Pandey <rpandey1234@gmail.com>
2013-05-20 03:55:24 -0700

    decouple image adapters for personal view and popular view

--- src/com/additt/browsing/PersonalAdsActivity.java
+++ src/com/additt/browsing/PersonalAdsActivity.java
@@ -1,31 +1,50 @@
 package com.additt.browsing;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.additt.maple_android.MapleApplication;
-import com.additt.maple_android.User;
+import com.additt.maple_android.MapleHttpClient;
 import com.additt.maple_android.R;
+import com.additt.maple_android.User;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.ProfilePictureView;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class PersonalAdsActivity extends BrowseActivity {
 
 	private ProfilePictureView mProfilePictureView;
+	// the view we are using to display the ads
+	private GridView mGridview;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setTitle("My Ads");
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_personal_ads);
-		GridView mGridview = (GridView) findViewById(R.id.gridviewAdsPersonal);
-		setGridview(mGridview);
+
+		mGridview = (GridView) findViewById(R.id.gridviewAdsPersonal);
+		
+		RequestParams params = new RequestParams();
+		User appUser = mApp.getUser();
+		if (appUser != null) {
+			params.put("auth_token", appUser.getAuthToken());
+			params.put("user_id", appUser.getId());
+		}
+		populateView(params, appUser.getAuthToken());
 		
 		Session session = Session.getActiveSession();
 		mProfilePictureView = (ProfilePictureView) findViewById(R.id.selection_profile_pic);
@@ -45,15 +64,50 @@ public class PersonalAdsActivity extends BrowseActivity {
 						}
 					});
 		}
-		MapleApplication mApp = (MapleApplication) this.getApplication();
-		User appUser = mApp.getUser();
 		
-		RequestParams params = new RequestParams();
-		// No params means just getting the most popular ads
-		params.put("user_id", appUser.getId());
-		params.put("auth_token", appUser.getAuthToken());
-		requestUserAds(params);
 	}
+	
+	public void populateView(RequestParams params, final String authToken) {
+		MapleHttpClient.get("posts", params, new AsyncHttpResponseHandler(){
+			// Example request: http://maplesyrup.herokuapp.com/posts?user_id=3
+			@Override
+			public void onSuccess(int statusCode, String response) {
+				ArrayList<DisplayAd> ads = new ArrayList<DisplayAd>();
+				try {
+					JSONArray jObjectAds = new JSONArray(response);
+			    	// Build up ArrayList of DisplayAds from JSON array
+			    	for (int i = 0; i < jObjectAds.length(); i++) {
+			    		DisplayAd dAd = new DisplayAd(jObjectAds.getJSONObject(i));
+			    		ads.add(dAd);
+			    	}
+			    	if (ads.size() == 0) {
+						((RelativeLayout) mGridview.getParent()).removeView(mGridview);
+						TextView adsTitle = (TextView) findViewById(R.id.adsTitle);
+						adsTitle.setText("There are no ads to show; you should create one!");
+						LayoutParams p = (LayoutParams) mGridview.getLayoutParams();
+						p.addRule(RelativeLayout.CENTER_HORIZONTAL);
+						adsTitle.setLayoutParams(p);
+						adsTitle.setTextSize(22);
+						adsTitle.setTypeface(null, Typeface.BOLD);
+					} else {
+						mGridview.setAdapter(new ImageAdapter(getApplicationContext(), ads, authToken));
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+					// TODO: sometimes mGridview.SetAdapter crashes with a null pointer. Which one of these
+					// is null in that case? Need to figure this bug out
+					System.out.println("application context:" + getApplicationContext());
+					System.out.println("authToken:" + mApp.getUser());
+				}
+			}
+			
+			@Override
+			public void onFailure(Throwable error, String response) {
+				Toast.makeText(getApplicationContext(), "Sugar! We ran into a problem fetching user ads!", Toast.LENGTH_LONG).show();
+		    }
+		});
+	}
+	
 	
 	@Override
 	public void onStart() {
--- src/com/additt/browsing/PopularAdsActivity.java
+++ src/com/additt/browsing/PopularAdsActivity.java
@@ -1,21 +1,35 @@
 package com.additt.browsing;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.additt.maple_android.MapleHttpClient;
 import com.additt.maple_android.R;
+import com.additt.maple_android.User;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class PopularAdsActivity extends BrowseActivity {
 
 	private static final String TAG = "PopularAds";
+	// the view we are using to display the ads
+	private GridView mGridview;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +37,13 @@ public class PopularAdsActivity extends BrowseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_popular_ads);
 		
-		GridView mGridview = (GridView) findViewById(R.id.gridviewAdsPopular);
-		Log.d(TAG, "found grid view");
+		mGridview = (GridView) findViewById(R.id.gridviewAdsPopular);
+		RequestParams params = new RequestParams();
+		User appUser = mApp.getUser();
+		if (appUser != null) {
+			params.put("auth_token", appUser.getAuthToken());
+		}
+		populateView(params, appUser.getAuthToken());
 		// On Click event for Single Gridview Item
         mGridview.setOnItemClickListener(new OnItemClickListener() {
             @Override
@@ -37,14 +56,6 @@ public class PopularAdsActivity extends BrowseActivity {
                 startActivity(i);
             }
         });
-		setGridview(mGridview);
-        
-		RequestParams params = new RequestParams();
-
-		if (mApp.getUser() != null) {
-			params.put("auth_token", mApp.getUser().getAuthToken());
-		}
-		requestUserAds(params);
 		
 		String success = getIntent().getStringExtra("successMessage");
 		if (success != null) {
@@ -53,6 +64,48 @@ public class PopularAdsActivity extends BrowseActivity {
 			toast.show();	
 		}
 		
+	}
+	
+	public void populateView(RequestParams params, final String authToken) {
+		MapleHttpClient.get("posts", params, new AsyncHttpResponseHandler(){
+			// Example request: http://maplesyrup.herokuapp.com/posts?user_id=3
+			@Override
+			public void onSuccess(int statusCode, String response) {
+				ArrayList<DisplayAd> ads = new ArrayList<DisplayAd>();
+				try {
+					JSONArray jObjectAds = new JSONArray(response);
+			    	// Build up ArrayList of DisplayAds from JSON array
+			    	for (int i = 0; i < jObjectAds.length(); i++) {
+			    		DisplayAd dAd = new DisplayAd(jObjectAds.getJSONObject(i));
+			    		Log.d(TAG, "Adding to arrlist");
+			    		ads.add(dAd);
+			    	}
+			    	if (ads.size() == 0) {
+						((RelativeLayout) mGridview.getParent()).removeView(mGridview);
+						TextView adsTitle = (TextView) findViewById(R.id.adsTitle);
+						adsTitle.setText("There are no ads to show; you should create one!");
+						LayoutParams p = (LayoutParams) mGridview.getLayoutParams();
+						p.addRule(RelativeLayout.CENTER_HORIZONTAL);
+						adsTitle.setLayoutParams(p);
+						adsTitle.setTextSize(22);
+						adsTitle.setTypeface(null, Typeface.BOLD);
+					} else {
+						mGridview.setAdapter(new ImageAdapter(getApplicationContext(), ads, authToken));
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+					// TODO: sometimes mGridview.SetAdapter crashes with a null pointer. Which one of these
+					// is null in that case? Need to figure this bug out
+					System.out.println("application context:" + getApplicationContext());
+					System.out.println("authToken:" + mApp.getUser());
+				}
+			}
+			
+			@Override
+		    public void onFailure(Throwable error, String response) {
+				Toast.makeText(getApplicationContext(), "Sugar! We ran into a problem fetching user ads!", Toast.LENGTH_LONG).show();
+		    }
+		});
 	}
 	
 	@Override
