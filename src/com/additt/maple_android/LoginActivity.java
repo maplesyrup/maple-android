f805497d63cf68ffd8c6db1f2b518121a1cfd6eb
Rahul Pandey <rpandey1234@gmail.com>
2013-05-28 02:34:43 -0700

    rewrite login activity

--- src/com/additt/maple_android/LoginActivity.java
+++ src/com/additt/maple_android/LoginActivity.java
@@ -2,35 +2,46 @@ package com.additt.maple_android;
 
 import org.json.JSONException;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.additt.browsing.PopularAdsActivity;
 import com.facebook.Session;
 import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
 
+	private static final int SPLASH = 0;
+	private static final int SELECTION = 1;
+	private static final int FRAGMENT_COUNT = SELECTION +1;
+
+	private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
 	private static final String TAG = "Login Activity";
+	private boolean isResumed = false;
 
-	private Session.StatusCallback statusCallback = new SessionStatusCallback();
-
+//	private Session.StatusCallback statusCallback = new SessionStatusCallback();
+	private UiLifecycleHelper uiHelper;
+	
 	private Button mButtonLoginLogout;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-
+		uiHelper = new UiLifecycleHelper(this, callback);
+		uiHelper.onCreate(savedInstanceState);
+		
 		// hide action bar for login page
 		try {
 			getActionBar().hide();
@@ -39,35 +50,99 @@ public class LoginActivity extends Activity {
 					"Well, not high enough SDK for action bar so it won't be shown anyways");
 		}
 
-		mButtonLoginLogout = (Button) findViewById(R.id.loginB);
-		mButtonLoginLogout.setOnClickListener(new OnClickListener() {
-			public void onClick(View view) {
-				onClickLogin();
-			}
-		});
-		Session session = Session.getActiveSession();
-		if (session == null) {
-			if (savedInstanceState != null) {
-				session = Session.restoreSession(this, null, statusCallback,
-						savedInstanceState);
-			}
-			if (session == null) {
-				session = new Session(this);
-			}
-			Session.setActiveSession(session);
-			if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
-				session.openForRead(new Session.OpenRequest(this)
-						.setCallback(statusCallback));
-			}
-		} else {
-			updateView();
-		}
+	    FragmentManager fm = getSupportFragmentManager();
+	    fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
+	    fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
+
+	    FragmentTransaction transaction = fm.beginTransaction();
+	    for(int i = 0; i < fragments.length; i++) {
+	        transaction.hide(fragments[i]);
+	    }
+	    transaction.commit();
+	}
+
+	private void showFragment(int fragmentIndex, boolean addToBackStack) {
+	    FragmentManager fm = getSupportFragmentManager();
+	    FragmentTransaction transaction = fm.beginTransaction();
+	    for (int i = 0; i < fragments.length; i++) {
+	        if (i == fragmentIndex) {
+	            transaction.show(fragments[i]);
+	        } else {
+	            transaction.hide(fragments[i]);
+	        }
+	    }
+	    if (addToBackStack) {
+	        transaction.addToBackStack(null);
+	    }
+	    transaction.commit();
+	}
+	
+	@Override
+	protected void onResumeFragments() {
+	    super.onResumeFragments();
+	    Session session = Session.getActiveSession();
+
+	    if (session != null && session.isOpened()) {
+	        // if the session is already open,
+	        // try to show the selection fragment
+			MapleApplication mApp = (MapleApplication) getApplication();
+			saveUserData(mApp, session.getAccessToken());
+	        showFragment(SELECTION, false);
+	    } else {
+	        // otherwise present the splash screen
+	        // and ask the person to login.
+	        showFragment(SPLASH, false);
+	    }
+	}
+	
+	private Session.StatusCallback callback = 
+	    new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    // Only make changes if the activity is visible
+	    if (isResumed) {
+	        FragmentManager manager = getSupportFragmentManager();
+	        // Get the number of entries in the back stack
+	        int backStackSize = manager.getBackStackEntryCount();
+	        // Clear the back stack
+	        for (int i = 0; i < backStackSize; i++) {
+	            manager.popBackStack();
+	        }
+	        if (state.isOpened()) {
+	            // If the session state is open:
+	            // Show the authenticated fragment
+	            showFragment(SELECTION, false);
+	        } else if (state.isClosed()) {
+	            // If the session state is closed:
+	            // Show the login fragment
+	            showFragment(SPLASH, false);
+	        }
+	    }
+	}
+	
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    uiHelper.onResume();
+	    isResumed = true;
 	}
 
 	@Override
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
+	    isResumed = false;
+	}
+	
+	@Override
 	public void onStart() {
 		super.onStart();
-		Session.getActiveSession().addCallback(statusCallback);
+		//Session.getActiveSession().addCallback(statusCallback);
 
 		// start analytics tracking for this activity
 		EasyTracker.getInstance().activityStart(this);
@@ -76,7 +151,7 @@ public class LoginActivity extends Activity {
 	@Override
 	public void onStop() {
 		super.onStop();
-		Session.getActiveSession().removeCallback(statusCallback);
+		//Session.getActiveSession().removeCallback(statusCallback);
 
 		// stop analytics tracking for this activity
 		EasyTracker.getInstance().activityStop(this);
@@ -85,49 +160,25 @@ public class LoginActivity extends Activity {
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode,
-				resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		Session session = Session.getActiveSession();
-		Session.saveSession(session, outState);
+		uiHelper.onSaveInstanceState(outState);
 	}
-
-	private void updateView() {
-		Session session = Session.getActiveSession();
-		if (session.isOpened()) {
-			saveUserData((MapleApplication) this.getApplication(),
-					session.getAccessToken());
-
-		}
-	}
-
-	private void onClickLogin() {
-		Session session = Session.getActiveSession();
-		if (!session.isOpened() && !session.isClosed()) {
-			session.openForRead(new Session.OpenRequest(this)
-					.setCallback(statusCallback));
-		} else {
-			Session.openActiveSession(this, true, statusCallback);
-		}
-	}
-
-	private class SessionStatusCallback implements Session.StatusCallback {
-		@Override
-		public void call(Session session, SessionState state,
-				Exception exception) {
-			updateView();
-		}
-	}
-
+	
 	/**
 	 * Retrieves user data from server based on access token
 	 * 
-	 * @param token
-	 *            The Facebook access token
+	 * @param token Facebook access token
 	 */
 	private void saveUserData(final MapleApplication mApp, final String token) {
 		RequestParams params = new RequestParams();
@@ -135,49 +186,33 @@ public class LoginActivity extends Activity {
 		MapleHttpClient.get("users/check_mobile_login", params,
 				new AsyncHttpResponseHandler() {
 
-					@Override
-					public void onStart() {
-						// update login button to say loading and disallow more
-						// clicks
-						Button loginB = (Button) findViewById(R.id.loginB);
-						loginB.setEnabled(false);
-						loginB.setBackgroundResource(R.drawable.loading);
-					}
+			@Override
+			public void onSuccess(int statusCode, String response) {
+				try {
+					User appUser = new User(response, token);
+					mApp.setUser(appUser);
+					Intent i = new Intent(LoginActivity.this,
+							PopularAdsActivity.class);
+					startActivity(i);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
 
-					@Override
-					public void onSuccess(int statusCode, String response) {
-						try {
-							User appUser = new User(response, token);
-							mApp.setUser(appUser);
-							Intent i = new Intent(LoginActivity.this,
-									PopularAdsActivity.class);
-							startActivity(i);
-						} catch (JSONException e) {
-							e.printStackTrace();
-						}
-					}
+			@Override
+			public void onFailure(Throwable error, String response) {
+				// Something went wrong so let's start up the
+				// LoginActivity again
+				// No let's not try again. This will throw us into an
+				// infinite loop.
+				Log.d(TAG, "Failed to login. Check heroku logs for error");
+				Toast.makeText(LoginActivity.this, "Failed to login", Toast.LENGTH_SHORT).show();
+			}
 
-					@Override
-					public void onFailure(Throwable error, String response) {
-						// Something went wrong so let's start up the
-						// LoginActivity again
-						// No let's not try again. This will throw us into an
-						// infinite loop.
-						Log.d(TAG,
-								"Failed to login. Check heroku logs for error");
-						Toast.makeText(LoginActivity.this, "Failed to login",
-								Toast.LENGTH_SHORT).show();
-
-					}
-					
-					@Override
-					public void onFinish(){
-						// restore the login button in case of failure, or
-						// if they return to this page
-						Button loginB = (Button) findViewById(R.id.loginB);
-						loginB.setEnabled(true);
-						loginB.setBackgroundResource(R.layout.button_sign_in);
-					}
-				});
+			@Override
+			public void onFinish(){
+				finish();
+			}
+		});
 	}
 }
--- src/com/additt/maple_android/SelectionFragment.java
+++ src/com/additt/maple_android/SelectionFragment.java
@@ -7,12 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SelectionFragment extends Fragment {
-	
+
 	private static final String TAG = "SelectionFragment";
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	    super.onCreateView(inflater, container, savedInstanceState);
-	    View view = inflater.inflate(R.layout.selection, container, false);
-	    return view;
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.selection, container, false);
+		return view;
 	}
+
 }
